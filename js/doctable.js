37575321141ec3f43d5043a0cfc09dbf9c4ac2a1
Margie Ruffin <margieruffin60@gmail.com>
2023-03-30 16:20:09 -0500

    adjustments to table

--- js/doctable.js
+++ js/doctable.js
@@ -1,23 +1,33 @@
 $(document).ready(function(){
-    $('#data').after('<div id="nav"></div>');
+    $('#data_table').before('<div id="nav"></div>');
     var rowsShown = 10;
-    var rowsTotal = $('#data tbody tr').length;
+    var rowsTotal = $('#data_table tbody tr').length;
     var numPages = rowsTotal/rowsShown;
     for(i = 0;i < numPages;i++) {
         var pageNum = i + 1;
         $('#nav').append('<a href="javascript:void()" rel="'+i+'">'+pageNum+'</a> ');
     }
-    $('#data tbody tr').hide();
-    $('#data tbody tr').slice(0, rowsShown).show();
-    $('#nav a:first').addClass('active');
+    $('#data_table tbody tr').hide();
+    $('#data_table tbody tr').slice(0, rowsShown).show();
+    $("#nav a").css({
+        fontSize: "25px",
+        color: "#4d9586",
+        paddingLeft: "5px",
+        paddingRight: "5px",
+        marginBottom: "10px"
+    });
+    $('#nav a:first').addClass('active').css("color", "white");
+
+
     $('#nav a').bind('click', function(){
 
-        $('#nav a').removeClass('active');
-        $(this).addClass('active');
+        $('#nav a').removeClass('active').css("color", "#4d9586");
+        $(this).addClass('active').css("color", "white");
         var currPage = $(this).attr('rel');
         var startItem = currPage * rowsShown;
         var endItem = startItem + rowsShown;
-        $('#data tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
-        css('display','table-row').animate({opacity:1}, 300);
+        $('#data_table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+        css('display','table-row').animate({opacity:1}, 500);
+        
     });
 });
