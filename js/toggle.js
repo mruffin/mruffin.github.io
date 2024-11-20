a2d03b9cf0f7149447b7968b1f0ba9b87101c47a
mruffin <margieruffin60@gmail.com>
2024-11-20 11:12:16 -0600

    update citations

---
+++ js/toggle.js
@@ -0,0 +1,168 @@
+window.onload = function() {
+    var x = document.getElementById("abs");
+    var y = document.getElementById("cite");
+
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }
+
+    if(y.style.display === "block"){
+        y.style.display = "none";
+    }
+}
+
+function absFunction() {
+    var x = document.getElementById("abs");
+    var y = document.getElementById("cite");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function citeFunction(){
+
+    var x = document.getElementById("abs");
+    var y = document.getElementById("cite");
+
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        x.style.display = "none";
+      } else {
+        y.style.display = "none";
+        
+      }
+}
+
+function absFunction1() {
+    var x = document.getElementById("abs1");
+    var y = document.getElementById("cite1");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function citeFunction1(){
+
+    var x = document.getElementById("abs1");
+    var y = document.getElementById("cite1");
+
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        x.style.display = "none";
+      } else {
+        y.style.display = "none";
+        
+      }
+}
+
+function absFunction2() {
+    var x = document.getElementById("abs2");
+    var y = document.getElementById("cite2");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function citeFunction2(){
+
+    var x = document.getElementById("abs2");
+    var y = document.getElementById("cite2");
+
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        x.style.display = "none";
+      } else {
+        y.style.display = "none";
+        
+      }
+}
+
+function absFunction3() {
+    var x = document.getElementById("abs3");
+    var y = document.getElementById("cite3");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function citeFunction3(){
+
+    var x = document.getElementById("abs3");
+    var y = document.getElementById("cite3");
+
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        x.style.display = "none";
+      } else {
+        y.style.display = "none";
+        
+      }
+}
+
+function absFunction4() {
+    var x = document.getElementById("abs4");
+    var y = document.getElementById("cite4");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function citeFunction4(){
+
+    var x = document.getElementById("abs4");
+    var y = document.getElementById("cite4");
+
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        x.style.display = "none";
+      } else {
+        y.style.display = "none";
+        
+      }
+}
+
+function absFunction5() {
+    var x = document.getElementById("abs5");
+    var y = document.getElementById("cite5");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function citeFunction5(){
+
+    var x = document.getElementById("abs5");
+    var y = document.getElementById("cite5");
+
+    if (y.style.display === "none") {
+        y.style.display = "block";
+        x.style.display = "none";
+      } else {
+        y.style.display = "none";
+        
+      }
+}
